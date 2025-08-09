9741eed88631e0d2896a28dbc9bfdf90ff8a3589
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2025-08-09 09:31:24 -0500

    build: Generatin .war File instead of .jar

--- src/main/java/com/cartagenacorp/lm_audit/LmAuditApplication.java
+++ src/main/java/com/cartagenacorp/lm_audit/LmAuditApplication.java
@@ -2,12 +2,19 @@ package com.cartagenacorp.lm_audit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LmAuditApplication {
+public class LmAuditApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmAuditApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LmAuditApplication.class);
 	}
 
 }
